04c0fbfa15ec4c6eae1b0dc74e8dbb88b22e24bc
NikkyXO <alexisdame2017@gmail.com>
2022-11-03 10:34:39 +0100

    updated API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import flask
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, abort
 from flask_cors import CORS
 
 
@@ -10,10 +10,13 @@ CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 
 
-details = {'slackUsername': 'Nikki',
+details = {'slackUsername': 'Nikky',
      'backend': True,
      'age': 28,
      'bio': 'Nikki was born in Lagos in 1994'}
+
+slackUsername = 'Nikky'
+
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -25,6 +28,42 @@ def home():
 def api_all():
     return jsonify(details)
 
+
+@app.route('/api/v1/operations', methods=['POST'], strict_slashes=False)
+def api_ops():
+    temp = request.get_json()
+
+    if not request.get_json():
+        abort(400, description='Not a JSON')
+    if 'operation_type' not in temp:
+        abort(400, description='Missing operation_type')
+    if 'x' not in temp:
+        abort(400, description='Missing x')
+    if 'y' not in temp:
+        abort(400, description='Missing y')
+
+    X = request.get('x')
+    Y = request.get('y')
+    operation_type = request.get('operation_type')
+    
+
+    if operation_type == 'addition':
+        temp =  X + Y
+    elif operation_type == 'subtraction':
+        temp = X - Y
+    elif operation_type == 'multiplication':
+        temp = X * Y
+    res = ['temp', 'slackUsername', 'operation_type']
+    data = {}
+    for m in res:
+        if m == 'temp':
+            data['temp'] = temp
+        elif m == 'slackUsername':
+            data['slackUsername'] = slackUsername
+        elif m == 'operation_type':
+            data['operation_type'] = operation_type
+
+    return make_response(jsonify(data), 200)
 
 if __name__ == '__main__':
     app.run()
